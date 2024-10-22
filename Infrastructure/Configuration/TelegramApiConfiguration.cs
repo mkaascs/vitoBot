22ea54dbb51992a3842471d28b1547f781002ad5
Max <teakeyx@gmail.com>
2024-10-22 16:39:55 +0300

    reset Infrastructure directory

--- Infrastructure/Configuration/TelegramApiConfiguration.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.Extensions.Configuration;
-
-namespace Infrastructure.Configuration;
-
-public class TelegramApiConfiguration(IConfiguration configuration) {
-    public string ApiKey { get; } = configuration["TelegramAPI:ApiKey"] 
-                                    ?? throw new InvalidOperationException("There is no api key in TelegramApi configuration");
-}
--- Infrastructure/Configuration/VitoApiConfiguration.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.Extensions.Configuration;
-
-namespace Infrastructure.Configuration;
-
-public class VitoApiConfiguration(IConfiguration configuration) {
-    public string DomainName { get; } = configuration["VitoAPI:DomainName"]
-                                        ?? throw new InvalidOperationException("There is no domain name in VitoApi configuration");
-}
